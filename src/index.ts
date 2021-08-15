e744e29590d5455be64219fb27bf3a388a29f3be
Nick Field <nicholas.field@iongroup.com>
2021-08-15 17:06:54 -0400

    try this

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import { execute } from './engine';
 
 var callerArgs = process.argv.slice(2);
-if (callerArgs.length < 2) {
+if (callerArgs.length < 3) {
     throw new Error('Insufficient arguments');
 }
 
 const tokensJSON = callerArgs[0];
 const types = callerArgs[1];
 
-const tokensDirectory = 'tokens';
-const buildDirectory = '/data';
+const tokensDirectory = 'jsontokens';
+const buildDirectory = callerArgs[2];
 
 execute(tokensJSON, types, tokensDirectory, buildDirectory);
 
