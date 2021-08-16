9bd4810a69716551794ee700173dae4ac4a66be9
Nick Field <nicholas.field@iongroup.com>
2021-08-15 20:10:28 -0400

    use uuid for directory

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import * as core from '@actions/core';
+import { v4 as uuidv4 } from 'uuid';
 import { execute } from './engine';
 
 const tokensJSON = core.getInput('tokens-json');
 const types = core.getInput('token-types');
 
-const tokensDirectory = 'jsontokens';
+const tokensDirectory = uuidv4();
 const buildDirectory = core.getInput('results-dir');
 
 execute(tokensJSON, types, tokensDirectory, buildDirectory);
